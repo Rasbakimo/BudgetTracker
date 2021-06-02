e81857a7b3c4cd229777de406823258c23e91df1
Rasbakimo <bakimocleckley@yahoo.com>
2021-06-02 18:17:20 -0400

    changed server js line 28

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3000;
+const PORT =process.env.PORT || 3000;
 
 const app = express();
 
@@ -16,16 +16,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budget',
-  {
+  process.env.MONGODB_URI || 'mongodb://localhost/budget', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
-  }
-);
+  });
 // routes
-require("./routes/api.js")(app);
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
